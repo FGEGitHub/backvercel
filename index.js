9e8761dd811fd96be161a366558cb953c84af0d8
FGEGitHub <pipao.pipo@gmail.com>
2024-01-24 12:56:45 -0300

    index 2

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
-// app.js
+const express = require('express')
+const morgan = require('morgan')
 
-// Importa el módulo 'express'
-const express = require('express');
+const path = require('path')
+const flash = require('connect-flash')
+const session = require('express-session')
+//const MySQLStore = require('express-mysql-session')
+const passport = require('passport')
+const cors = require("cors");
+const jwt = require('jsonwebtoken')
+const keys = require('./src/keys')
 
-// Crea una instancia de la aplicación express
-const app = express();
-const PORT = 3000; // Puedes cambiar el puerto si lo deseas
 
-// Define una ruta de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
+////
 
-// Inicia el servidor en el puerto especificado
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+
+
+//inicializacion
+const app = express()
+require('./src/lib/passport')
+app.set('key', keys.key)
+
+//settings
+
+app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views')) // indica donde esta la carpeta views 
+
+
+app.set('view engine', '.hbs')
+
+
+//middlwares
+/* app.use(session({
+    secret: 'faztmysqlnodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+})) */
+
+app.use(flash())
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false })) // para recibir datos de formularios
+app.use(express.json())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cors());
+
+
+
+//globalvariables
+app.use((req, res, next) => {
+    app.locals.success = req.flash('success')
+    app.locals.success = req.flash('message')
+    app.locals.user = req.user
+    next();
+})
+
+
+//routes
+app.use(require('./src/routes/index'))
+//app.use(require('./routes/authentication'))
+
+
+
+app.use(express.static(path.join(__dirname, './pdfs')))
+app.use(express.static(path.join(__dirname, 'pdfs')))
+//public  
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.static(path.join(__dirname, 'dbimages')))
+
+
+//start 
+app.listen(app.get('port'), () => {
+    console.log(`server onport`, app.get('port'))
+})
+
